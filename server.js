fded72ecc192f5ed893f48a51eeca94ede891e1e
Samuel Ting <samuelting7@gmail.com>
2026-04-05 18:26:13 +0800

    feat: change server to listen to 0.0.0.0 instead of localhost

--- server.js
+++ server.js
@@ -75,3 +75,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
